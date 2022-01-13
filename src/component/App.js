a396a275464800dd486faa9c0120a8d9906f9056
Kamil Ertekin <48670775+Tasarimoloji@users.noreply.github.com>
2022-01-13 10:37:00 +0100

    todov4

--- src/component/App.js
+++ src/component/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import ToDoItem from "./ToDoItem";
 import todosData from "./todosData";
 
-function App() {
-  const Items = todosData.map((item) => {
-    return <ToDoItem key={item.id} item={item} />;
-  });
-
-  return <div>{Items}</div>;
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      todos:todosData
+    }
+  }
+  render(){
+    const Items = this.state.todos.map((item) => {
+      return <ToDoItem key={item.id} item={item} />;
+    return(
+       <div>{Items}</div>;
+    )
+  }
 }
 
 export default App;
