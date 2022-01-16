8f296da31b78abf069cd246109e63ba79c3f3175
Kamil Ertekin <48670775+Tasarimoloji@users.noreply.github.com>
2022-01-16 09:08:44 +0100

    forms details

--- src/component/App.js
+++ src/component/App.js
@@ -6,15 +6,20 @@ class App extends React.Component {
     this.state = {
       firstName: "",
       lastName: "",
+      isFriendly: false,
+      gender: "",
+      favColor: "green",
     }
     this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange(event) {
-    const { name, value } = event.target
-    this.setState({
-      [name]: value,
-    })
+    const { name, value, type, checked } = event.target
+    type === "checkbox"
+      ? this.setState({ [name]: checked })
+      : this.setState({
+          [name]: value,
+        })
   }
   render() {
     return (
@@ -33,9 +38,58 @@ class App extends React.Component {
           placeholder="Last Name"
           onChange={this.handleChange}
         />
+        <textarea value={"Some default value bruh"} onChange={this.handleChange} />
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            name="isFriendly"
+            checked={this.state.isFriendly}
+            onChange={this.handleChange}
+          />{" "}
+          Is friendly
+        </label>
+        <br />
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            checked={this.state.gender === "male"}
+            onChange={this.handleChange}
+          />{" "}
+          Male
+        </label>
+        <br />
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            checked={this.state.gender === "female"}
+            onChange={this.handleChange}
+          />{" "}
+          Female
+        </label>
+        <br />
+
+        <label>Favorite Color</label>
+        <select
+          value={this.state.favColor}
+          name="favColor"
+          onChange={this.handleChange}
+        >
+          <option value="blue">Blue</option>
+          <option value="green">Green</option>
+          <option value="red">Red</option>
+        </select>
+
+        <br />
         <h1>
           {this.state.firstName} {this.state.lastName}
         </h1>
+        <h2>You are a {this.state.gender}</h2>
+        <h2>Favorite Color : {this.state.favColor}</h2>
       </form>
     )
   }
