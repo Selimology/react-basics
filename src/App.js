16b14d67728cb93cd22c8b9328fcc605e8b20e10
Kamil Ertekin <48670775+Tasarimoloji@users.noreply.github.com>
2022-03-01 19:20:52 +0100

    addContact and Update it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import React from "react"
-import Header from "./Header"
-import MemeGenerator from "./MemeGenerator"
+import React, { useState } from "react"
 
 function App() {
+  const [inputData, setInputData] = useState({ firstName: "", lastName: "" })
+  const [contactsData, setContactsData] = useState([])
+
+  function handleChange(event) {
+    const { name, value } = event.target
+    setInputData((prevInputData) => {
+      return {
+        ...prevInputData,
+        [name]: value,
+      }
+    })
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    setContactsData((prevContacts) => {
+      return [...prevContacts, inputData]
+    })
+  }
+
+  const contacts = contactsData.map((contact) => {
+    return (
+      <h2 key={contact.firstName + contact.lastName}>
+        First Name: {contact.firstName} Last Name: {contact.lastName}{" "}
+      </h2>
+    )
+  })
   return (
-    <div>
-      <Header />
-      <MemeGenerator />
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="FirstName"
+          name="firstName"
+          value={inputData.firstName}
+          onChange={handleChange}
+        />
+        <input
+          placeholder="Last Name"
+          name="lastName"
+          value={inputData.lastName}
+          onChange={handleChange}
+        />
+
+        <button>add contact</button>
+      </form>
+      {contacts}
+    </>
   )
 }
 export default App
