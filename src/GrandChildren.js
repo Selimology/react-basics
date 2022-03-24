fb79269b3e01df8949a2acb4af1d67b8d7f55df8
Kamil Ertekin <48670775+Tasarimoloji@users.noreply.github.com>
2022-03-24 17:31:28 +0100

    PureComponent

--- src/GrandChildren.js
+++ src/GrandChildren.js
@@ -1,15 +1,6 @@
-import React, { Component, Pure } from "react"
+import React, { Component } from "react"
 
 class GrandChildren extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    //in this case it is false, because both of them
-    // are null or undefined
-    if (nextProps.count === this.props.count) {
-      return false //if it returns false, it won'update
-    }
-    return true // if it returns true, it will update render
-  }
-
   render() {
     console.log("[][][][GC] ")
     return (
--- src/GrandParent.js
+++ src/GrandParent.js
@@ -1,7 +1,15 @@
-import React, { Component } from "react"
+import React, { PureComponent } from "react"
 import Parent from "./Parent"
 
-class GrandParent extends Component {
+class GrandParent extends PureComponent {
+  /* 
+    
+    PureComponent does the job of shouldComponentUpdate,
+    it automatically compares two different objects
+    and returns false and true accordingly.
+    
+*/
+
   render() {
     console.log("[GP][][][] ")
     return (
